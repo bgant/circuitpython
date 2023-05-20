d4f668b5663474dfe5699562dd6841d624dfbc07
Brandon Gant <bgant@protonmail.com>
2023-05-19 22:23:49 -0500

    pool monitor project

--- pool-monitor/code.py
+++ pool-monitor/code.py
@@ -9,16 +9,26 @@ display = DISPLAY(
     busy  = board.GP26,
     )
 
+# Rounding the way you expect in Math
+def roundTraditional(val,digits):
+   return round(val+10**(-len(str(val))-1), digits)
+
+# Read thermocouple water temperature
+from my_adafruit_thermocouple import thermocouple
+sensor = thermocouple(sck=board.GP18, mosi=board.GP19, miso=board.GP16, cs=board.GP17)
+sensor.averaging = 8
+thermoTempF = (sensor.temperature * 9.0/5.0) + 32
+print(f"Thermocouple Temp: {thermoTempF:.1f}°F")
+water = int(roundTraditional(thermoTempF, 0))
+
+
 
 air   = 79
-water = 86
-
-
 # Account for size of 3-digit Air Temperatures
 if air > 99:
-    digits = -10
+    air_digits = -10
 else:
-    digits = 14
+    air_digits = 14
 
 
 # Create a display group for our screen objects
@@ -27,7 +37,7 @@ from my_image_functions import draw_background_color, draw_text, draw_image
 image_buffer = Group()
 image_buffer.append(draw_background_color(width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT, color=0xffffff))
 image_buffer.append(draw_image('/pool.bmp', x=0, y=0))
-image_buffer.append(draw_text(string=str(air), scale=1, x=digits, y=50, color=0x000000, font="/GothamBlack-54.bdf"))
+image_buffer.append(draw_text(string=str(air), scale=1, x=air_digits, y=50, color=0x000000, font="/GothamBlack-54.bdf"))
 image_buffer.append(draw_text(string="Air", scale=1, x=40, y=95, color=0x000000, font="/GothamBlack-25.bdf"))
 image_buffer.append(draw_text(string=str(water), scale=1, x=14, y=185, color=0x000000, font="/GothamBlack-54.bdf"))
 image_buffer.append(draw_text(string="Water", scale=1, x=20, y=231, color=0x000000, font="/GothamBlack-25.bdf"))
