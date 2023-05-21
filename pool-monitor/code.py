a93ab954aa1a9a9be18e509e43b9013c50eb2ab5
Brandon Gant <bgant@protonmail.com>
2023-05-20 22:19:31 -0500

    pool monitor project

--- pool-monitor/code.py
+++ pool-monitor/code.py
@@ -5,7 +5,20 @@
 ###################################
 import board
 from time import sleep
+import wifi
 from displayio import Group
+import ssl
+import socketpool
+import gc
+
+###################################
+# Import Library Bundle Functions
+###################################
+import adafruit_requests
+# adafruit_bitmap_font
+# adafruit_display_text
+# adafruit_max31856
+# adafruit_ssd1680
 
 
 ###################################
@@ -14,11 +27,24 @@ from displayio import Group
 from my_adafruit_thermocouple import thermocouple
 from my_adafruit_2in13_eInk import DISPLAY, DISPLAY_WIDTH, DISPLAY_HEIGHT
 from my_image_functions import draw_background_color, draw_text, draw_image
+try:
+    from secrets import secrets # secrets.py file with key:value pairs (see note above)
+except:
+    print('You need to create a secrets.py file... See comments at the top of this script.')
 
 
 ###################################
 # Initialize Wifi
 ###################################
+wifi.radio.connect(secrets["ssid"], secrets["password"])
+
+
+###################################
+# Initialize HTTPS
+###################################
+pool = socketpool.SocketPool(wifi.radio)
+https = adafruit_requests.Session(pool, ssl.create_default_context())
+JSON_URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + secrets['lat'] + "&lon=" + secrets['lon'] + "&units=imperial&appid=" + secrets['appid']
 
 
 ###################################
@@ -50,8 +76,8 @@ def roundTraditional(val,digits):
 
 def water_reading():
     thermoTempF = (sensor.temperature * 9.0/5.0) + 32
-    print(f'Thermocouple Temp: {thermoTempF:.1f}°F')
-    return int(roundTraditional(thermoTempF, 0))
+    print(f'Thermocouple Temp: {int(roundTraditional(thermoTempF,0))}°F')
+    return int(roundTraditional(thermoTempF,0))
 
 
 ###################################
@@ -59,7 +85,13 @@ def water_reading():
 ###################################
 
 def air_reading():
-    return 79
+    response = https.get(JSON_URL)
+    json_data = response.json()
+    response.close()
+    #print(json_data)
+    feels_like = json_data['main']['feels_like']
+    print(f'Air Feels Like:    {int(roundTraditional(feels_like,0))}°F')
+    return int(roundTraditional(feels_like,0))
     
 # Account for size of 3-digit Air Temperatures
 def air_digits(air=None):
@@ -92,6 +124,10 @@ def main():
         print('Readings have not changed... No Display Update...')
         return  # Exit without continuing
 
+    # Update Last Values before next Loop
+    last_air = air
+    last_water = water
+
     # Create a display group for our screen objects
     image_buffer = Group()
     image_buffer.append(draw_background_color(width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT, color=0xffffff))
@@ -107,16 +143,12 @@ def main():
     while display.busy:
         pass  # Wait until all display processing is complete
     
-    # Update Last Values before next Loop
-    last_air = air
-    last_water = water
-
 
 ###################################
 # Main Running Loop
 ###################################
 
-sleep_interval = 120  # Time between loops
+sleep_interval = 180  # Time between loops
 
 while True:
     try:
@@ -124,8 +156,10 @@ while True:
         print(f'Sleeping for {sleep_interval} seconds...')
         print('')
         sleep(sleep_interval)
+        while display.time_to_refresh > 0:  # Just in case sleep_interval is too short
+            pass  
     except Exception as error:
-        print('ERROR:', error)
+        print(f'ERROR: {error} in {__file__}')
         print(f'Sleeping for {sleep_interval} seconds before Reset...')
         sleep(sleep_interval)
         from supervisor import reload
